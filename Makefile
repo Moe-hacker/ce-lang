34d56401515b812b5f725ac3b9d80bd3d134b31c
Moe-hacker <moe-hacker@outlook.com>
2026-06-21 23:06:52 +0800

    add print_nautilus()

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
-fmt:
+all :
+	cargo build --release
+	cp target/release/cwte .
+fmt :
 	sed -i "s/:</_CE_PAN/g" t.ce
 	sed -i "s/:>/_CE_DFT/g" t.ce
 	clang-format -i --assume-filename=t.c t.ce 
@@ -10,3 +13,6 @@ fmt:
 	sed -i "s/:</_CE_PAN/g" test.ce
 	clang-format -i --assume-filename=t.c test.ce
 	sed -i "s/_CE_PAN/:</g" test.ce
+test:
+	cargo build
+	cp target/debug/cwte .
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+fn print_nautilus(file: &str, line_no: usize, content: &str) {
+    println!("Cwte tail at {} line {}:", file, line_no);
+    println!(">>");
+    println!(">>  {}", content);
+    println!(">>");
+    // Cooked by rust at the beginning, now I cry.
+    // `}` should be `}}` in rust fmt.
+    // I miss my cprintf now.
+    println!("::}} Here's a nautilus, have an ice cream and write a fix, or it'll become a fossil QwQ");
+}
 
 fn main() {
-    println!("Hello, world!");
+    print_nautilus("test.ce", 4, "foo() ::};");
 }
