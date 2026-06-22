add0a7e654c55ac0d385a4d77656dccff9a82a2d
Moe-hacker <moe-hacker@outlook.com>
2026-06-22 20:31:42 +0800

    add @ce_line_xx@ metadata

--- src/main.rs
+++ src/main.rs
@@ -67,6 +67,37 @@ fn print_nautilus(file: &str, line_no: usize, content: &str, enforce: bool) {
         panic!("Cwte ::}} tail is enforced, you must fix this before compiling.");
     }
 }
+fn prepare_layer(mut input: File) -> File {
+    /*
+     * prepare, add ce_line_xx mark to each line.
+     */
+    // Seek to the beginning of the file.
+    input
+        .seek(std::io::SeekFrom::Start(0))
+        .expect("Failed to seek input file");
+    // Read input to string.
+    let mut content = String::new();
+    input
+        .read_to_string(&mut content)
+        .expect("Failed to read input file");
+    // memfd magic!
+    let fd = memfd_create(
+        "cwte_output",
+        MemfdFlags::CLOEXEC | MemfdFlags::ALLOW_SEALING,
+    )
+    .expect("Failed to create memfd");
+    let mut mfd_file = fs::File::from(fd);
+    // Now, erase the `::}` in content, and print the nautilus for it.
+    for (i, line) in content.lines().enumerate() {
+        // Or, write the line to the output file.
+        writeln!(mfd_file, "@ce_line_{}@{}", i + 1, line).expect("Failed to write to file");
+    }
+    // Make the memfd immutable to prevent further modification.
+    mfd_file.sync_all().expect("Failed to sync memfd");
+    fcntl_add_seals(mfd_file.as_fd(), SealFlags::WRITE).expect("Failed to add seals to memfd");
+    // Return the memfd file for further processing.
+    mfd_file
+}
 fn nautilus_layer(mut input: File, file: &str) -> File {
     /*
      * Nautilus mark ::} is cwte TODO mark.
@@ -161,6 +192,49 @@ fn linter_layer(mut input: File, file: &str) -> File {
     // Return the memfd file for further processing.
     mfd_file
 }
+fn final_layer(mut input: File) -> File {
+    /*
+     * Final, remove @ce_line_xx@ mark.
+     */
+    // Seek to the beginning of the file.
+    input
+        .seek(std::io::SeekFrom::Start(0))
+        .expect("Failed to seek input file");
+    // Read input to string.
+    let mut content = String::new();
+    input
+        .read_to_string(&mut content)
+        .expect("Failed to read input file");
+    // memfd magic!
+    let fd = memfd_create(
+        "cwte_output",
+        MemfdFlags::CLOEXEC | MemfdFlags::ALLOW_SEALING,
+    )
+    .expect("Failed to create memfd");
+    let mut mfd_file = fs::File::from(fd);
+    // Now, erase the `::}` in content, and print the nautilus for it.
+    for line in content.lines() {
+        // The line_no is now untrustable.
+        // So we just match first @ and second @, and erase it.
+        if let Some(start) = line.find('@') {
+            // Check if the first character is @, if not, this line is unmarked, just write it to the output file.
+            if line[start..].starts_with('@') {
+                if let Some(end) = line[start + 1..].find('@') {
+                    let fixed = format!("{}{}", &line[..start], &line[start + end + 2..]);
+                    writeln!(mfd_file, "{}", fixed).expect("Failed to write to file");
+                    continue;
+                }
+            }
+        }
+        // Or, write the line to the output file.
+        writeln!(mfd_file, "{}", line).expect("Failed to write to file");
+    }
+    // Make the memfd immutable to prevent further modification.
+    mfd_file.sync_all().expect("Failed to sync memfd");
+    fcntl_add_seals(mfd_file.as_fd(), SealFlags::WRITE).expect("Failed to add seals to memfd");
+    // Return the memfd file for further processing.
+    mfd_file
+}
 fn main() {
     /*
      * We will never release any memfd file, kernel will help us do that.
@@ -176,10 +250,14 @@ fn main() {
     }
     // Open the input file.
     let input_file = fs::File::open(&args[1]).expect("Failed to open input file");
+    // Process the input file with prepare layer, and get the memfd file.
+    let after_prepare = prepare_layer(input_file);
     // Process the input file with nautilus layer, and get the memfd file.
-    let mut mfd_file = nautilus_layer(input_file, &args[1]);
+    let mut mfd_file = nautilus_layer(after_prepare, &args[1]);
     // Process the memfd file with linter layer, and get the new memfd file.
     mfd_file = linter_layer(mfd_file, &args[1]);
+    // Process the memfd file with final layer, and get the new memfd file.
+    mfd_file = final_layer(mfd_file);
     // Write the content of memfd to the output file.
     let output_file = format!("{}.c", args[1]);
     let mut output = fs::File::create(&output_file).expect("Failed to create output file");
